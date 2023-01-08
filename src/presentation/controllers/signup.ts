1944ac1f74b46a544575b27fb18f8472687fe11c
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2023-01-08 09:58:21 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,5 @@
 import { Controller, EmailValidator, HttpResponse, HttRequest } from '../protocols'
-import { InvalidParamError, MissingParamError } from '../errors'
+import { InvalidParamError, MissingParamError, ServerError } from '../errors'
 import { badRequest } from '../helpers'
 
 export class SignUpController implements Controller {
@@ -10,14 +10,21 @@ export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
 
   handle (httpRequest: HttRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isValid) return badRequest(new InvalidParamError('email'))
+      return badRequest(new MissingParamError('default'))
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
       }
     }
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
-    if (!isValid) return badRequest(new InvalidParamError('email'))
-    return badRequest(new MissingParamError('default'))
   }
 }
---
+++ src/presentation/errors/server-error.ts
@@ -0,0 +1,6 @@
+export class ServerError extends Error {
+  constructor () {
+    super('Um erro inesperado ocorreu. tente novamente dentro de alguns minutos.')
+    this.name = 'InvalidParamError'
+  }
+}
