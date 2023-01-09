c824d63dbb48d5eb8a7c414aa9508fcfc95b6012
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2023-01-09 11:57:13 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -18,7 +18,7 @@ const makeAddAccountRepository = (): AddAccountRepository => {
         id: 'valid_id',
         name: 'valid_name',
         email: 'valid_email@email.com',
-        password: 'encrypted_password'
+        password: 'hashed_password'
       }
       return await new Promise(resolve => { resolve(fakeAccount) })
     }
@@ -94,4 +94,20 @@ test('Should throw if AddAccountRepository throws', async () => {
   }
   const promise = sut.add(accountData)
   await expect(promise).rejects.toThrow()
+})
+
+test('Should return an account on success', async () => {
+  const { sut } = makeSut()
+  const accountData = {
+    name: 'valid_name',
+    email: 'valid_email@email.com',
+    password: 'valid_password'
+  }
+  const account = await sut.add(accountData)
+  expect(account).toEqual({
+    id: 'valid_id',
+    name: 'valid_name',
+    email: 'valid_email@email.com',
+    password: 'hashed_password'
+  })
 })
