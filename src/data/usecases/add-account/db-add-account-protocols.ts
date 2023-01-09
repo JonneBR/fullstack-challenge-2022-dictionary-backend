2c5707b6e1751ee324c52f141f30c7a3834e5fd6
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2023-01-09 11:26:55 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,3 +1,4 @@
 export * from '@/domain/models/account'
 export * from '@/domain/usecases/add-account'
 export * from '@/data/protocols/encrypter'
+export * from '@/data/protocols/add-account-repository'
