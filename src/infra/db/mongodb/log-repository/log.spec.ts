6cc9f95f33327a839c3e1eacf3e2d072bce86b6e
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2023-01-12 14:57:08 -0300

    test: ensure LogMongoRepository creates an error long on success

---
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -0,0 +1,25 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../mongo-helper'
+import { LogMongoRepository } from './log'
+
+let errorCollection: Collection
+
+beforeAll(async () => {
+  await MongoHelper.connect(process.env.MONGO_URL)
+})
+
+afterAll(async () => {
+  await MongoHelper.disconnect()
+})
+
+beforeEach(async () => {
+  errorCollection = MongoHelper.getCollection('errors')
+  await errorCollection.deleteMany({})
+})
+
+test('Should create an error log on success', async () => {
+  const sut = new LogMongoRepository()
+  await sut.log('any_error')
+  const count = await errorCollection.countDocuments()
+  expect(count).toBe(1)
+})
